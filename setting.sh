858c2c8f79c90ba555b8bb770756664e718e8ea9
diannao <55k@outlook.com>
2023-04-07 01:00:02 +0800

    同步参数配置

--- setting.sh
+++ setting.sh
@@ -2,16 +2,16 @@
 export logging=True
 # 日志
 
-export glm_path="model/chatglm-6b"
+export glm_path="model/chatglm-6b-int4"
 # glm模型位置
 
 
 export rwkv_path="model/RWKV-4-Raven-7B-v7-ChnEng-20230404-ctx2048.pth"
 # rwkv模型位置
-export rwkv_strategy="cuda fp16"
+export rwkv_strategy="cuda fp16i8 *18+"
 # rwkv模型参数
 
-export chunk_size=100
+export chunk_size=200
 # chunk_size
-export chunk_count=3
+export chunk_count=1
 # chunk_count
