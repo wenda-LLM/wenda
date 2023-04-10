04a90db91ba0967a80259eb5e1b1d77e3a89e11c
l15y <ly444983@live.com>
2023-04-10 10:53:29 +0800

    优化打印

--- plugins/settings.py
+++ plugins/settings.py
@@ -6,30 +6,40 @@ print('WebUI 端口号',port)
 red="\033[1;32m"
 green="\033[1;31m"
 white="\033[1;37m"
-glm_path=os.environ.get('glm_path')
-print('glm模型地址',glm_path)
-glm_strategy =os.environ.get('glm_strategy')
-print('glm模型参数',glm_strategy)
-if 'int4' in glm_path:
-    # 判断glm_strategy是否以'i4'或'i8'结尾
-    if glm_strategy.endswith('i4') or glm_strategy.endswith('i8'):
-        # 报错并退出程序
-        print('Error: 请不要使用预量化的模型再设置开始量化参数')
-        exit()
+llm_type =os.environ.get('llm_type')
+print('LLM模型类型',llm_type)
+def load_LLM():
+    try:
+        from importlib import import_module
+        LLM = import_module('plugins.llm_'+llm_type)
+        return LLM
+    except  Exception as e:
+        print("LLM模型加载失败，请阅读说明：https://github.com/l15y/wenda",e)
+if llm_type=="glm6b":
+    glm_path=os.environ.get('glm_path')
+    print('glm模型地址',glm_path)
+    glm_strategy =os.environ.get('glm_strategy')
+    print('glm模型参数',glm_strategy)
+    if 'int4' in glm_path:
+        # 判断glm_strategy是否以'i4'或'i8'结尾
+        if glm_strategy.endswith('i4') or glm_strategy.endswith('i8'):
+            # 报错并退出程序
+            print('Error: 请不要使用预量化的模型再设置开始量化参数')
+            exit()
 
-glm_lora_path = os.environ.get('glm_lora_path')
-if not (glm_lora_path == '' or glm_lora_path == None):
-    print('glm LoRA 微调启用: ', glm_lora_path)
+    glm_lora_path = os.environ.get('glm_lora_path')
+    if not (glm_lora_path == '' or glm_lora_path == None):
+        print('glm LoRA 微调启用: ', glm_lora_path)
 
-rwkv_path =os.environ.get('rwkv_path')
-print('rwkv模型地址',rwkv_path)
-rwkv_strategy =os.environ.get('rwkv_strategy')
-print('rwkv模型参数',rwkv_strategy)
-
-rwkv_lora_path = os.environ.get('rwkv_lora_path')
-if not (rwkv_lora_path == '' or rwkv_lora_path == None):
-    print('rwkv LoRA 微调启用: ', rwkv_lora_path)
-    rwkv_lora_alpha = os.environ.get('rwkv_lora_alpha')
+if llm_type=="rwkv":
+    rwkv_path =os.environ.get('rwkv_path')
+    print('rwkv模型地址',rwkv_path)
+    rwkv_strategy =os.environ.get('rwkv_strategy')
+    print('rwkv模型参数',rwkv_strategy)
+    rwkv_lora_path = os.environ.get('rwkv_lora_path')
+    if not (rwkv_lora_path == '' or rwkv_lora_path == None):
+        print('rwkv LoRA 微调启用: ', rwkv_lora_path)
+        rwkv_lora_alpha = os.environ.get('rwkv_lora_alpha')
 logging =os.environ.get('logging')!="0"
 print('日志记录',logging)
 
@@ -47,16 +57,6 @@ def load_zsk():
         return zhishiku
     except  Exception as e:
         print("知识库加载失败，请阅读说明：https://github.com/l15y/wenda",e)
-llm_type =os.environ.get('llm_type')
-print('LLM模型类型',llm_type)
-def load_LLM():
-    try:
-        from importlib import import_module
-        LLM = import_module('plugins.llm_'+llm_type)
-        return LLM
-    except  Exception as e:
-        print("LLM模型加载失败，请阅读说明：https://github.com/l15y/wenda",e)
-
 
 chunk_size =int(os.environ.get('chunk_size'))
 print('chunk_size',chunk_size)
