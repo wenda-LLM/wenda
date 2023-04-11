13f4d573c76e2aad12c09af0b099ab3be3b11549
Leon <zoulion@126.com>
2023-04-11 11:13:09 +0800

    Fix: Format codes.

--- plugins/settings.py
+++ plugins/settings.py
@@ -1,25 +1,29 @@
 import os
 
-port=os.environ.get('PORT')
-print('WebUI 端口号',port)
+port = os.environ.get('PORT')
+print('WebUI 端口号', port)
 
-red="\033[1;32m"
-green="\033[1;31m"
-white="\033[1;37m"
-llm_type =os.environ.get('llm_type')
-print('LLM模型类型',llm_type)
+red = "\033[1;32m"
+green = "\033[1;31m"
+white = "\033[1;37m"
+llm_type = os.environ.get('llm_type')
+print('LLM模型类型', llm_type)
+
+
 def load_LLM():
     try:
         from importlib import import_module
         LLM = import_module('plugins.llm_'+llm_type)
         return LLM
-    except  Exception as e:
-        print("LLM模型加载失败，请阅读说明：https://github.com/l15y/wenda",e)
-if llm_type=="glm6b":
-    glm_path=os.environ.get('glm_path')
-    print('glm模型地址',glm_path)
-    glm_strategy =os.environ.get('glm_strategy')
-    print('glm模型参数',glm_strategy)
+    except Exception as e:
+        print("LLM模型加载失败，请阅读说明：https://github.com/l15y/wenda", e)
+
+
+if llm_type == "glm6b":
+    glm_path = os.environ.get('glm_path')
+    print('glm模型地址', glm_path)
+    glm_strategy = os.environ.get('glm_strategy')
+    print('glm模型参数', glm_strategy)
     if 'int4' in glm_path:
         # 判断glm_strategy是否以'i4'或'i8'结尾
         if glm_strategy.endswith('i4') or glm_strategy.endswith('i8'):
@@ -31,37 +35,40 @@ if llm_type=="glm6b":
     if not (glm_lora_path == '' or glm_lora_path == None):
         print('glm LoRA 微调启用: ', glm_lora_path)
 
-if llm_type=="rwkv":
-    rwkv_path =os.environ.get('rwkv_path')
-    print('rwkv模型地址',rwkv_path)
-    rwkv_strategy =os.environ.get('rwkv_strategy')
-    print('rwkv模型参数',rwkv_strategy)
+if llm_type == "rwkv":
+    rwkv_path = os.environ.get('rwkv_path')
+    print('rwkv模型地址', rwkv_path)
+    rwkv_strategy = os.environ.get('rwkv_strategy')
+    print('rwkv模型参数', rwkv_strategy)
     rwkv_lora_path = os.environ.get('rwkv_lora_path')
     if not (rwkv_lora_path == '' or rwkv_lora_path == None):
         print('rwkv LoRA 微调启用: ', rwkv_lora_path)
         rwkv_lora_alpha = os.environ.get('rwkv_lora_alpha')
-logging =os.environ.get('logging')!="0"
-print('日志记录',logging)
+logging = os.environ.get('logging') != "0"
+print('日志记录', logging)
 
-zsk_type =os.environ.get('zsk_type')
-print('知识库类型',zsk_type)
-if zsk_type=='x':
-    embeddings_path =os.environ.get('embeddings_path')
-    print('embeddings模型地址',embeddings_path)
-    vectorstore_path =os.environ.get('vectorstore_path')
-    print('vectorstore保存地址',vectorstore_path)
+zsk_type = os.environ.get('zsk_type')
+print('知识库类型', zsk_type)
+if zsk_type == 'x':
+    embeddings_path = os.environ.get('embeddings_path')
+    print('embeddings模型地址', embeddings_path)
+    vectorstore_path = os.environ.get('vectorstore_path')
+    print('vectorstore保存地址', vectorstore_path)
+
+
 def load_zsk():
     try:
         from importlib import import_module
         zhishiku = import_module('plugins.zhishiku_'+zsk_type)
         return zhishiku
-    except  Exception as e:
-        print("知识库加载失败，请阅读说明：https://github.com/l15y/wenda",e)
+    except Exception as e:
+        print("知识库加载失败，请阅读说明：https://github.com/l15y/wenda", e)
 
-chunk_size =int(os.environ.get('chunk_size'))
-print('chunk_size',chunk_size)
-chunk_count =int(os.environ.get('chunk_count'))
-print('chunk_count',chunk_count)
 
-zhishiku_folder_name =os.environ.get('zsk_folder', "txt")
+chunk_size = int(os.environ.get('chunk_size'))
+print('chunk_size', chunk_size)
+chunk_count = int(os.environ.get('chunk_count'))
+print('chunk_count', chunk_count)
+
+zhishiku_folder_name = os.environ.get('zsk_folder', "txt")
 print('知识库文件夹名称为:', zhishiku_folder_name)
--- wenda.py
+++ wenda.py
@@ -1,61 +1,84 @@
-import threading,os,json
-import datetime
-from bottle import route, response, request,static_file,hook
-import bottle
-from plugins import settings
-LLM=settings.load_LLM()
 import torch
+import threading
+import os
+import json
+import datetime
+from bottle import route, response, request, static_file, hook
+import bottle
+
+from plugins import settings
+LLM = settings.load_LLM()
 if settings.logging:
     from plugins.defineSQL import session_maker, 记录
 mutex = threading.Lock()
+
+
 @route('/static/<path:path>')
 def staticjs(path='-'):
     return static_file(path, root="views/static/")
+
+
 @route('/:name')
 def static(name='-'):
     return static_file(name, root="views")
+
+
 @route('/')
 def index():
-    response.set_header( "Pragma", "no-cache" ); 
-    response.add_header( "Cache-Control", "must-revalidate" ); 
-    response.add_header( "Cache-Control", "no-cache" ); 
-    response.add_header( "Cache-Control", "no-store" ); 
+    response.set_header("Pragma", "no-cache")
+    response.add_header("Cache-Control", "must-revalidate")
+    response.add_header("Cache-Control", "no-cache")
+    response.add_header("Cache-Control", "no-store")
     return static_file("index.html", root="views")
-当前用户=None
+
+
+当前用户 = None
+
+
 @route('/api/chat_now', method='GET')
 def api_chat_now():
     return '当前状态：'+当前用户[0]
+
+
 @hook('before_request')
 def validate():
     REQUEST_METHOD = request.environ.get('REQUEST_METHOD')
-    HTTP_ACCESS_CONTROL_REQUEST_METHOD = request.environ.get('HTTP_ACCESS_CONTROL_REQUEST_METHOD')
+    HTTP_ACCESS_CONTROL_REQUEST_METHOD = request.environ.get(
+        'HTTP_ACCESS_CONTROL_REQUEST_METHOD')
     if REQUEST_METHOD == 'OPTIONS' and HTTP_ACCESS_CONTROL_REQUEST_METHOD:
         request.environ['REQUEST_METHOD'] = HTTP_ACCESS_CONTROL_REQUEST_METHOD
+
+
 @route('/api/save_news', method='OPTIONS')
 @route('/api/save_news', method='POST')
 def api_chat_stream():
     response.set_header('Access-Control-Allow-Origin', '*')
-    response.add_header('Access-Control-Allow-Methods','POST,OPTIONS')
+    response.add_header('Access-Control-Allow-Methods', 'POST,OPTIONS')
     response.add_header('Access-Control-Allow-Headers',
                         'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token')
     try:
         data = request.json
-        if not data:return '0'
+        if not data:
+            return '0'
         title = data.get('title')
         txt = data.get('txt')
-        cut_file=f"txt/{title}.txt"
-        with open(cut_file, 'w',encoding='utf-8') as f:   
+        cut_file = f"txt/{title}.txt"
+        with open(cut_file, 'w', encoding='utf-8') as f:
             f.write(txt)
             f.close()
         return '1'
     except Exception as e:
         print(e)
     return '2'
+
+
 @route('/api/find', method='POST')
 def api_find():
     data = request.json
     prompt = data.get('prompt')
     return json.dumps(zhishiku.find(prompt))
+
+
 @route('/api/chat_stream', method='POST')
 def api_chat_stream():
     data = request.json
@@ -75,63 +98,76 @@ def api_chat_stream():
         use_zhishiku = False
     history = data.get('history')
     history_formatted = LLM.chat_init(history)
-    response=''
+    response = ''
     # print(request.environ)
-    IP=request.environ.get('HTTP_X_REAL_IP') or request.environ.get('REMOTE_ADDR')
+    IP = request.environ.get(
+        'HTTP_X_REAL_IP') or request.environ.get('REMOTE_ADDR')
     global 当前用户
-    error=""
+    error = ""
     with mutex:
-        footer='///'
+        footer = '///'
         yield str(len(prompt))+'字正在计算'
         if use_zhishiku:
             if keyword is None:
-                keyword=prompt
+                keyword = prompt
             # print(keyword)
-            response_d=zhishiku.find(keyword)
-            torch.cuda.empty_cache() 
+            response_d = zhishiku.find(keyword)
+            torch.cuda.empty_cache()
             output_sources = [i['title'] for i in response_d]
-            results ='\n---\n'.join([i['content'] for i in response_d])
-            prompt=  'system:学习以下文段, 用中文回答用户问题。如果无法从中得到答案，忽略文段内容并用中文回答用户问题。\n\n'+results+'\nuser:'+prompt
-            footer=  "\n来源：\n"+('\n').join(output_sources)+'///'
+            results = '\n---\n'.join([i['content'] for i in response_d])
+            prompt = 'system:学习以下文段, 用中文回答用户问题。如果无法从中得到答案，忽略文段内容并用中文回答用户问题。\n\n' + \
+                results+'\nuser:'+prompt
+            footer = "\n来源：\n"+('\n').join(output_sources)+'///'
         yield footer
-        
-        print( "\033[1;32m"+IP+":\033[1;31m"+prompt+"\033[1;37m")
+
+        print("\033[1;32m"+IP+":\033[1;31m"+prompt+"\033[1;37m")
         try:
-            for response in LLM.chat_one(prompt,history_formatted,max_length,top_p,temperature,zhishiku=use_zhishiku):
-                if(response):yield response+footer
+            for response in LLM.chat_one(prompt, history_formatted, max_length, top_p, temperature, zhishiku=use_zhishiku):
+                if (response):
+                    yield response+footer
         except Exception as e:
-            error=str(e)
-            print("错误",settings.red,error,settings.white,e)
-            response=''
-        torch.cuda.empty_cache() 
-    if response=='':
-            yield "发生错误，正在重新加载模型"+error+'///'
-            os._exit(0)
+            error = str(e)
+            print("错误", settings.red, error, settings.white, e)
+            response = ''
+        torch.cuda.empty_cache()
+    if response == '':
+        yield "发生错误，正在重新加载模型"+error+'///'
+        os._exit(0)
     if settings.logging:
         with session_maker() as session:
-            jl = 记录(时间=datetime.datetime.now(),IP=IP,问= prompt,答=response)
+            jl = 记录(时间=datetime.datetime.now(), IP=IP, 问=prompt, 答=response)
             session.add(jl)
             session.commit()
     print(response)
     yield "/././"
-model=None
-tokenizer=None
+
+
+model = None
+tokenizer = None
+
+
 def load_model():
     global 当前用户
     mutex.acquire()
-    当前用户=['模型加载中','','']
+    当前用户 = ['模型加载中', '', '']
     LLM.load_model()
     mutex.release()
-    torch.cuda.empty_cache() 
-    print(settings.green,"模型加载完成",settings.white)
+    torch.cuda.empty_cache()
+    print(settings.green, "模型加载完成", settings.white)
+
+
 thread_load_model = threading.Thread(target=load_model)
 thread_load_model.start()
-zhishiku=None
+zhishiku = None
+
+
 def load_zsk():
     global zhishiku
-    zhishiku=settings.load_zsk()
-    print(settings.green,"知识库加载完成",settings.white)
+    zhishiku = settings.load_zsk()
+    print(settings.green, "知识库加载完成", settings.white)
+
+
 thread_load_zsk = threading.Thread(target=load_zsk)
 thread_load_zsk.start()
 bottle.debug(True)
-bottle.run(server='paste',host="0.0.0.0",port=settings.port,quiet=True)
+bottle.run(server='paste', host="0.0.0.0", port=settings.port, quiet=True)
