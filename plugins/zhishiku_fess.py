ed4fdf4c9dc9ad681c65eb5721a44ab302299abc
l15y <ly444983@live.com>
2023-04-15 10:08:53 +0800

    Update zhishiku_fess.py

--- plugins/zhishiku_fess.py
+++ plugins/zhishiku_fess.py
@@ -14,13 +14,17 @@ def replace_strong(s):
     s=re.sub(r'</strong>', "", s)
     return s
 def find(search_query):
-    tr4w.analyze(text=search_query, lower=True, window=2) 
-    search_query=' '.join([i['word'] for i in tr4w.get_keywords(20, word_min_len=1)])
-    print("关键词：",search_query)
-    url = 'http://127.0.0.1:8080/json/?q={}&num=10&sort=score.desc&lang=zh_CN'.format(search_query)
-    res = session.get(url, headers=headers, proxies=proxies)
-    r = res.json()
-    r=r["response"]['result']
-    # "<strong>""</strong>"
-    return [{'title': r[i]['title'], 'content':replace_strong(r[i]['content_description'])}
-            for i in range(min(settings.chunk_count, len(r)))]
+    try:
+        tr4w.analyze(text=search_query, lower=True, window=2) 
+        search_query=' '.join([i['word'] for i in tr4w.get_keywords(20, word_min_len=1)])
+        print("关键词：",search_query)
+        url = 'http://127.0.0.1:8080/json/?q={}&num=10&sort=score.desc&lang=zh_CN'.format(search_query)
+        res = session.get(url, headers=headers, proxies=proxies)
+        r = res.json()
+        r=r["response"]['result']
+        # "<strong>""</strong>"
+        return [{'title': r[i]['title'], 'content':replace_strong(r[i]['content_description'])}
+                for i in range(min(settings.chunk_count, len(r)))]
+    except Exception  as e:
+        print("fess读取失败",e)
+        return []
