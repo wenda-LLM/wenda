9595e56a5859abc5ef1404789696a30966946e88
l15y <ly444983@live.com>
2023-04-16 23:39:43 +0800

    -

--- 闻达auto油猴脚本形式示例/rwkv写论文脚本升级版-作者：FIGHTZERO.user.js
+++ 闻达auto油猴脚本形式示例/rwkv写论文脚本升级版-作者：FIGHTZERO.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         写论文脚本升级版-rwkv
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  写论文脚本升级版-rwkv
+// @author       FIGHTZERO
 // @match        http://127.0.0.1:17860/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=0.1
 // @grant        none
