5a350a22bf692b2263e088710e0425598753d378
l15y <ly444983@live.com>
2023-04-16 11:34:22 +0800

    优化分词

--- plugins/textrank4zh/__init__.py
+++
@@ -1,8 +0,0 @@
-#-*- encoding:utf-8 -*-
-from __future__ import absolute_import
-from .TextRank4Keyword import TextRank4Keyword
-from .TextRank4Sentence import TextRank4Sentence
-from . import Segmentation
-from . import util
-
-version = '0.2'
--- plugins/zhishiku_fess.py
+++ plugins/zhishiku_fess.py
@@ -1,9 +1,12 @@
 import requests
 import re,json
 from plugins.settings import settings
-from plugins.textrank4zh import TextRank4Keyword, TextRank4Sentence
-
-tr4w = TextRank4Keyword()
+# encoding=utf-8
+import jieba
+ 
+seg_list = jieba.cut_for_search("小明硕士毕业于中国科学院计算所，后在日本京都大学深造")  # 搜索引擎模式
+print(" ".join(seg_list))
+ 
 session = requests.Session()
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31'}
@@ -15,8 +18,7 @@ def replace_strong(s):
     return s
 def find(search_query):
     try:
-        tr4w.analyze(text=search_query, lower=True, window=2) 
-        search_query=' '.join([i['word'] for i in tr4w.get_keywords(20, word_min_len=1)])
+        search_query=' '.join(jieba.cut(search_query) )
         print("关键词：",search_query)
         url = 'http://' + settings.library.fess.Fess_Host + '/json/?q={}&num=10&sort=score.desc&lang=zh_CN'.format(search_query)
         res = session.get(url, headers=headers, proxies=proxies)
