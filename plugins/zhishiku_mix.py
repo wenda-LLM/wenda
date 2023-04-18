d706d1927d74cf9502a91788c6fec9357a55814e
diannao <55k@outlook.com>
2023-04-19 00:51:05 +0800

    知识库支持上下文step设置

--- plugins/zhishiku_mix.py
+++ plugins/zhishiku_mix.py
@@ -7,9 +7,9 @@ for item in input_list:
     from importlib import import_module
     zhishiku = import_module('plugins.zhishiku_'+item[0])
     zsk.append({'zsk':zhishiku,"count":int(item[1])})
-def find(s):
+def find(s,step = 0):
     result=[]
     for item in zsk:
-        result+=item['zsk'].find(s)[:item['count']]
+        result+=item['zsk'].find(s,step)[:item['count']]
     return result[:int(settings.library.mix.Count)]
 
