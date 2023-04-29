642043a53360adaff9cbd297c3c6ee1a03cf32e0
l15y <ly444983@live.com>
2023-04-29 09:12:08 +0800

    优化自动设置环境

--- envirment.bat
+++ envirment.bat
@@ -1,14 +1,22 @@
 chcp 65001
 title 闻达
+set "WINPYDIR=%~dp0\WPy64-31110\python-3.11.1.amd64"
+IF EXIST %WINPYDIR% (
+echo 检测到新版集成环境，使用内置Python解释器
+set "PATH=%WINPYDIR%\;%WINPYDIR%\DLLs;%WINPYDIR%\Scripts;%PATH%;"
+set "PYTHON=%WINPYDIR%\python.exe "
+goto end
+) 
 set "WINPYDIR=%~dp0\WPy64-38100\python-3.8.10.amd64"
 IF EXIST %WINPYDIR% (
-echo 检测到集成环境，使用内置Python解释器
+echo 检测到旧版集成环境，使用内置Python解释器
 set "PATH=%WINPYDIR%\;%WINPYDIR%\DLLs;%WINPYDIR%\Scripts;%PATH%;"
 set "PYTHON=%WINPYDIR%\python.exe "
 goto end
 ) 
 IF EXIST python (
 echo 未检测到集成环境，使用系统Python解释器
+set "PYTHON=python.exe "
 )ELSE (
 )
 :end
--- run_moss.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call envirment.bat
-:a
-%PYTHON% wenda.py -t moss
-goto a
-pause
-exit /b
