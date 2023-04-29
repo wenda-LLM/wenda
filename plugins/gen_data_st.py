d58f562f63e82014edceeefbea39bc1228c12263
l15y <ly444983@live.com>
2023-04-29 17:14:53 +0800

    rtst取代st；数据预处理流式进行，解决oom问题

--- plugins/gen_data_st.py
+++ plugins/gen_data_st.py
@@ -5,6 +5,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.document_loaders import DirectoryLoader
 from langchain.vectorstores.faiss import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.docstore.document import Document
 import pdfplumber
 import re
 import os
@@ -26,28 +27,50 @@ from settings import settings
 from settings import error_helper 
 from settings import success_print 
 
-source_folder = settings.library.st.Path
-target_folder = source_folder + '_out'
+source_folder = 'txt'
 source_folder_path = os.path.join(os.getcwd(), source_folder)
-target_folder_path = os.path.join(os.getcwd(), target_folder)
 
-if not os.path.exists(target_folder_path):
-    os.mkdir(target_folder_path)
 
 root_path_list = source_folder_path.split(os.sep)
+docs=[]
+vectorstore=None
 
-print("1.预处理数据 1/10")
+model_path=settings.library.rtst.Model_Path
+try:
+    embeddings = HuggingFaceEmbeddings(model_name='')
+    embeddings.client = sentence_transformers.SentenceTransformer(
+        model_path, device="cuda")
+except Exception as e:
+    error_helper("embedding加载失败，请下载相应模型",
+                          r"https://github.com/l15y/wenda#st%E6%A8%A1%E5%BC%8F")
+    raise e
+
+def make_index():
+    global vectorstore,docs
+    text_splitter = CharacterTextSplitter(
+        chunk_size=20, chunk_overlap=0, separator='\n')
+    doc_texts = text_splitter.split_documents(docs)
+    docs=[]
+    texts = [d.page_content for d in doc_texts]
+    metadatas = [d.metadata for d in doc_texts]
+    vectorstore_new = FAISS.from_texts(texts, embeddings, metadatas=metadatas)
+    if vectorstore is None:
+        vectorstore=vectorstore_new
+    else:
+        vectorstore.merge_from(vectorstore_new)
 for root, dirs, files in os.walk(source_folder_path):
     path_list = root.split(os.sep)
     for file in files:
-        if file.__contains__(".pdf"):
+        data=""
+        title=""
+        if file.endswith(".pdf"):
             file_path = os.path.join(root,file)
             with pdfplumber.open(file_path) as pdf:
                 data_list = []
                 for page in pdf.pages:
                     data_list.append(page.extract_text())
                 data = "\n".join(data_list)
-            cut_file_name = file.replace("pdf","txt")
+            title = file.replace(".pdf","")
         else:
             # 其他支持，这里是txt
             try:
@@ -58,49 +81,24 @@ for root, dirs, files in os.walk(source_folder_path):
                 file_path = os.path.join(root, file)
                 with open(file_path, "r", encoding='utf-8') as f:
                     data = f.read()
-            data = re.sub(r'！', "！\n", data)
-            data = re.sub(r'：', "：\n", data)
-            data = re.sub(r'。', "。\n", data)
-            data = re.sub(r'[\n\r]+', "\n", data)
-            filename_prefix_list = [
-                item for item in path_list if item not in root_path_list]
-            file_name_prefix = '_'.join(x for x in filename_prefix_list if x)
-            cut_file_name = file_name_prefix + '_' + file if file_name_prefix else file
-        cut_file_path = os.path.join(target_folder_path, cut_file_name)
-        with open(cut_file_path, 'w', encoding='utf-8') as f:
-            f.write(data)
-            f.close() # with open 可以不用close
-
-print("2.预处理完成 2/10")
-loader = DirectoryLoader(target_folder, glob='**/*.txt')
-print("3.从文件夹 "+target_folder+" 载入所有txt数据... 3/10")
-docs = loader.load()
-print("4.完成数据结构化处理... 4/10")
-text_splitter = CharacterTextSplitter(
-    chunk_size=int(settings.library.st.Size), chunk_overlap=int(settings.library.st.Overlap), separator='\n')
-print("5.构建分句器... 5/10")
-doc_texts = text_splitter.split_documents(docs)
-print("6.完成句分割... 6/10")
-try:
-    embeddings = HuggingFaceEmbeddings(model_name='')
-    embeddings.client = sentence_transformers.SentenceTransformer(
-        settings.library.st.Model_Path, device="cuda")
-except Exception as e:
-    error_helper("embedding加载失败，请下载相应模型",
-                          r"https://github.com/l15y/wenda#st%E6%A8%A1%E5%BC%8F")
-print("7.完成句向量处理... 7/10")
-
-texts = [d.page_content for d in doc_texts]
-metadatas = [d.metadata for d in doc_texts]
-vectorstore = FAISS.from_texts(texts, embeddings, metadatas=metadatas)
-print("8.处理完成 8/10")
+            title = file.replace(".txt","")
+        data = re.sub(r'[\n\r]+', "", data)
+        data = re.sub(r'！', "！\n", data)
+        data = re.sub(r'：', "：\n", data)
+        data = re.sub(r'。', "。\n", data)
+        print("读取",len(data),title)
+        docs.append(Document(page_content=data, metadata={"source":title }))
+        if len(docs)>100:
+            make_index()
+if len(docs)>0:
+    make_index()
 try:
     vectorstore_old = FAISS.load_local(
-        'vectorstore_path', embeddings=embeddings)
-    print("9.合并至已有索引。如不需合并请删除 vectorstore_path 文件夹 9/10")
+        'memory/default', embeddings=embeddings)
+    print("9.合并至已有索引。如不需合并请删除 memory/default 文件夹 9/10")
     vectorstore_old.merge_from(vectorstore)
-    vectorstore_old.save_local('vectorstore_path')
+    vectorstore_old.save_local('memory/default')
 except:
     print("9.新建索引 9/10")
-    vectorstore.save_local('vectorstore_path')
+    vectorstore.save_local('memory/default')
 success_print("10.保存完成 10/10")
