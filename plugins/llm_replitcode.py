6c569371866be5ff886f9eefa550470525452907
l15y <ly444983@live.com>
2023-05-05 09:42:49 +0800

    Update llm_replitcode.py

--- plugins/llm_replitcode.py
+++ plugins/llm_replitcode.py
@@ -1,20 +1,7 @@
 from plugins.common import settings
 
 def chat_init(history):
-    history_formatted = None
-    if history is not None:
-        history_formatted = []
-        tmp = []
-        for i, old_chat in enumerate(history):
-            if len(tmp) == 0 and old_chat['role'] == "user":
-                tmp.append(old_chat['content'])
-            elif old_chat['role'] == "AI" or old_chat['role'] == 'assistant':
-                tmp.append(old_chat['content'])
-                history_formatted.append(tuple(tmp))
-                tmp = []
-            else:
-                continue
-    return history_formatted
+    return []
 
 
 def chat_one(prompt, history_formatted, max_length, top_p, temperature, zhishiku=False):
