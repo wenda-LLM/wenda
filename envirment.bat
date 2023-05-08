26ec869eb401f19b2f3c19de54905685fb589e73
l15y <ly444983@live.com>
2023-05-08 18:54:34 +0800

    windows下自动关闭快速编辑模式，防止大神暂停了还说程序有bug

--- envirment.bat
+++ envirment.bat
@@ -1,5 +1,8 @@
 chcp 65001
 title 闻达
+reg add HKEY_CURRENT_USER\Console /v QuickEdit /t REG_DWORD /d 00000000 /f
+rem 关闭快速编辑模式，防止大神暂停了还说程序有bug
+cls
 set "WINPYDIR=%~dp0\WPy64-31110\python-3.11.1.amd64"
 IF EXIST %WINPYDIR% (
 echo 检测到新版集成环境，使用内置Python解释器
