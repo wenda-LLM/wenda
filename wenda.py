d287791fc292be6d3d6eb0da84a4319f49f2de88
l15y <ly444983@live.com>
2023-05-11 00:18:31 +0800

    Update wenda.py

--- wenda.py
+++ wenda.py
@@ -246,12 +246,18 @@ def api_chat_stream():
     if use_zhishiku:
         # print(keyword)
         response_d = zhishiku.find(keyword, int(settings.library.step))
-        output_sources = [i['title'] for i in response_d]
-        results = '\n'.join([str(i+1)+". "+re.sub('\n\n', '\n',
-                            response_d[i]['content']) for i in range(len(response_d))])
-        prompt = 'system: 请扮演一名专业分析师，根据以下内容回答问题：'+prompt + "\n" + results
-        if settings.library.show_soucre == True:
-            footer = "\n### 来源：\n"+('\n').join(output_sources)+'///'
+        if len(response_d)==0:
+            use_zhishiku=False
+            prompt = 'system: 请扮演一名专业分析师，根据以下内容回答问题：'+prompt + "\n" + results
+            if settings.library.show_soucre == True:
+                footer = "\n### 未查找到资料///"
+        else:
+            output_sources = [i['title'] for i in response_d]
+            results = '\n'.join([str(i+1)+". "+re.sub('\n\n', '\n',
+                                response_d[i]['content']) for i in range(len(response_d))])
+            prompt = 'system: 请扮演一名专业分析师，根据以下内容回答问题：'+prompt + "\n" + results
+            if settings.library.show_soucre == True:
+                footer = "\n### 来源：\n"+('\n').join(output_sources)+'///'
     with mutex:
         print("\033[1;32m"+IP+":\033[1;31m"+prompt+"\033[1;37m")
         try:
