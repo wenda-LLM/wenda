dd947149062409bd7eba15657e4a73552ca3c10b
sailorJoe <591428779@qq.com>
2023-05-10 18:02:25 +0800

    mix增加指定rtst库

--- plugins/zhishiku_mix.py
+++ plugins/zhishiku_mix.py
@@ -8,10 +8,18 @@ for item in input_list:
     zhishiku = import_module('plugins.zhishiku_'+item[0])
     if zhishiku is None:
         error_print("载入知识库失败",item[0])
-    zsk.append({'zsk':zhishiku,"count":int(item[1])})
+    if item[0] == "rtst" and len(item) > 2:
+        zsk.append({'zsk': zhishiku, "count": int(item[1]), "rtst_mem_name": item[2]})
+    else:
+        zsk.append({'zsk': zhishiku, "count": int(item[1])})
 def find(s,step = 0):
     result=[]
     for item in zsk:
         result+=item['zsk'].find(s,step)[:item['count']]
+    for item in zsk:
+        if "rtst_mem_name" in item:
+            result += item['zsk'].find(s, step, item['rtst_mem_name'])[:item['count']]
+        else:
+            result += item['zsk'].find(s, step)[:item['count']]
     return result[:int(settings.library.mix.count)]
 
