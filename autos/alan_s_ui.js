e40e64cc9200f117b201752d29f3fb4fbed15b4f
l15y <ly444983@live.com>
2023-05-18 20:48:39 +0800

    Create alan_s_ui.js

---
+++ autos/alan_s_ui.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://127.0.0.1:17860/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=0.1
+// @grant        none
+// ==/UserScript==
+app.plugins.push({ icon: "newspaper-variant", url: "/static/wenda-webui/index.html" })
