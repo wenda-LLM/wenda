9656354533648cb9af76ac58727e0b44969c8d4a
l15y <ly444983@live.com>
2023-05-25 11:07:10 +0800

    autos支持隐藏标题

--- autos/alan_s_ui.js
+++ autos/alan_s_ui.js
@@ -8,4 +8,4 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=0.1
 // @grant        none
 // ==/UserScript==
-app.plugins.push({ icon: "newspaper-variant", url: "/alan_ui.html" })
+app.plugins.push({ icon: "newspaper-variant", url: "/alan_ui.html",hide_title:true })
