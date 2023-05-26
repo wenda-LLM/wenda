1408258d355f1afb63eba2ef9ed3202af197a8e9
l15y <ly444983@live.com>
2023-05-26 19:38:59 +0800

    auto管理器支持在线增加脚本；编程猫支持在线增加脚本

--- autos/block_programming.js
+++ autos/block_programming.js
@@ -9,24 +9,34 @@
 // @grant        none
 // ==/UserScript==
 
-app.plugins.push({ icon: "cat", url: "static/blockly.html" ,hide_title:true})
+app.plugins.push({ icon: "cat", url: "static/blockly.html", hide_title: true })
 window.addEventListener('message', function (e) {
     if (e.data.from == '猫猫也会的图块化编程') {
         let data = e.data.data
-        data="async ()=>{"+data+"}"
+        data = "async ()=>{" + data + "}"
         console.log(data)
         load_feature({
             名称: "猫猫也会的图块化编程",
             描述: "",
-            问题:eval(data)
+            问题: eval(data)
         })
+        this.alert("载入成功")
+    }
+    if (e.data.from == '猫猫也会的图块化编程_保存') {
+        let data = e.data.data
+        data = `// @name 猫猫编程${Date.now()}
+        功能.push({
+            名称: "猫猫编程${Date.now()}",
+            描述: "根据主题撰写内容翔实、有信服力的论文",
+            问题: async () => {`+ data + "} })"
+        add_auto(data)
         this.alert("载入成功")
     }
     if (e.data.from == '猫猫也会的图块化编程_分析') {
         let data = e.data.data
-        data=data.replace(/window\.alert/g,"alert").replace(/await/g,"")
+        data = data.replace(/window\.alert/g, "alert").replace(/await/g, "")
         lsdh(false)
-        send("我正在教小朋友使用编程，请分析下列由blocky生成的程序实现的功能:\n"+data, keyword = "请分析我写的程序")
-        app.tab=0
+        send("我正在教小朋友使用编程，请分析下列由blocky生成的程序实现的功能:\n" + data, keyword = "请分析我写的程序")
+        app.tab = 0
     }
 });
