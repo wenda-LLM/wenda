d1664fe7b6eaffe6b5b2d0ace7cd76da646b19f1
l15y <ly444983@live.com>
2023-06-02 20:33:20 +0800

    快速知识库知识未查找到回退普通对话

--- autos/0-zsk.js
+++ autos/0-zsk.js
@@ -81,9 +81,18 @@ func.push({
             url: get_url_form_md(i.title),
             content: i.content
         }))
-        let prompt = app.zsk_answer_prompt + '\n' +
-            kownladge.map((e, i) => i + 1 + "." + e.content).join('\n') + "\n问题：" + Q
-        await send(prompt, keyword = Q, show = true, sources = kownladge)
+        if (kownladge.length > 0) {
+            let prompt = app.zsk_answer_prompt + '\n' +
+                kownladge.map((e, i) => i + 1 + "." + e.content).join('\n') + "\n问题：" + Q
+            await send(prompt, keyword = Q, show = true, sources = kownladge)
+        } else {
+            app.chat.pop()
+            sources = [{
+                title: '未匹配到知识库',
+                content: '本次对话内容完全由模型提供'
+            }]
+            return await send(Q, keyword = Q, show = true, sources = sources)
+        }
     }
 }
 )
