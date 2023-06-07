c0252ff93623bad3a41e73d5c826375a78c99e06
l15y <ly444983@live.com>
2023-06-07 10:45:08 +0800

    bug fix

--- llms/convert_rwkv.py
+++ llms/convert_rwkv.py
@@ -1,4 +1,4 @@
 from rwkv.model import RWKV
-model_name="model\\RWKV-4-World-7B-v1-OnlyForTest_30%_trained-20230529-ctx4096.pth"
+model_name="model\\RWKV-4-World-7B-v1-OnlyForTest_52%_trained-20230606-ctx4096.pth"
 RWKV(model=model_name, strategy='cuda fp16i8',
  convert_and_save_and_exit =model_name.replace(".pth","-i8.pth"))
