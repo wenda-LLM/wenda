80442df7b021393fa0763f4b99b2d2f877de6ef2
l15y <ly444983@live.com>
2023-06-15 21:05:08 +0800

    初步支持aquila的hf版

--- llms/llm_aquila.py
+++ llms/llm_aquila.py
@@ -58,55 +58,8 @@ def load_model():
     print(s)
     from transformers import AutoTokenizer, AutoModelForCausalLM
 
-    tokenizer = AutoTokenizer.from_pretrained(settings.llm.path)
-    model = AutoModelForCausalLM.from_pretrained(settings.llm.path, trust_remote_code=True)
-
-        
-    device, precision = s[0][0], s[0][1]
-    # 根据设备执行不同的操作
-    if device == 'cpu':
-        # 如果是cpu，不做任何操作
-        pass
-    elif device == 'cuda':
-        # 如果是gpu，把模型移动到显卡
-        import torch
-        if not (precision.startswith('fp16i') and torch.cuda.get_device_properties(0).total_memory < 1.4e+10):
-            model = model.cuda()
-    elif len(s)>1 and device.startswith('cuda:'):
-        pass
-    else:
-        # 如果是其他设备，报错并退出程序
-        print('Error: 不受支持的设备')
-        exit()
-    # 根据精度执行不同的操作
-    if precision == 'fp16':
-        # 如果是fp16，把模型转化为半精度
-        model = model.half()
-    elif precision == 'fp32':
-        # 如果是fp32，把模型转化为全精度
-        model = model.float()
-    elif precision.startswith('fp16i'):
-        # 如果是fp16i开头，把模型转化为指定的精度
-        # 从字符串中提取精度的数字部分
-        bits = int(precision[5:])
-        # 调用quantize方法，传入精度参数
-        model = model.quantize(bits)
-        if device == 'cuda':
-            model = model.cuda()
-        model = model.half()
-    elif precision.startswith('fp32i'):
-        # 如果是fp32i开头，把模型转化为指定的精度
-        # 从字符串中提取精度的数字部分
-        bits = int(precision[5:])
-        # 调用quantize方法，传入精度参数
-        model = model.quantize(bits)
-        if device == 'cuda':
-            model = model.cuda()
-        model = model.float()
-    else:
-        # 如果是其他精度，报错并退出程序
-        print('Error: 不受支持的精度')
-        exit()
-    if len(s)>1:
-        model = dispatch_model(model, device_map=device_map)
+    tokenizer = AutoTokenizer.from_pretrained(settings.llm.path, local_files_only=True,)
+    model = AutoModelForCausalLM.from_pretrained(settings.llm.path, local_files_only=True,)
+    model = model.half()
+    model = model.cuda()
     model = model.eval()
