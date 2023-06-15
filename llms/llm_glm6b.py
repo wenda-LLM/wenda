7de7279487916cf7f90c63bb0a73a6e21697f9e0
l15y <ly444983@live.com>
2023-06-15 21:15:44 +0800

    Update llm_glm6b.py

--- llms/llm_glm6b.py
+++ llms/llm_glm6b.py
@@ -64,9 +64,9 @@ def load_model():
             device_map[f'transformer.layers.{i}'] = n[i]
 
     tokenizer = AutoTokenizer.from_pretrained(
-        settings.llm.path, local_files_only=True,revision="v1.1.0")
+        settings.llm.path, local_files_only=True, trust_remote_code=True,revision="v1.1.0")
     model = AutoModel.from_pretrained(
-        settings.llm.path, local_files_only=True,revision="v1.1.0")
+        settings.llm.path, local_files_only=True, trust_remote_code=True,revision="v1.1.0")
     if not (settings.llm.lora == '' or settings.llm.lora == None):
         print('Lora模型地址', settings.llm.lora)
         from peft import PeftModel
