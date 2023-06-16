cf07941b5f7f60a915896f2072d96b69367bbf21
l15y <ly444983@live.com>
2023-06-16 19:07:19 +0800

    编程猫支持使用知识库回答

--- autos/0-zsk.js
+++ autos/0-zsk.js
@@ -69,31 +69,31 @@ func.push({
         answer_with_zsk(app.question)
     }
 })
+window.answer_with_fast_zsk = async () => {
+    let Q = app.question
 
+    // lsdh(false)
+    kownladge = (await find(Q, app.zsk_step)).map(i => ({
+        title: get_title_form_md(i.title),
+        url: get_url_form_md(i.title),
+        content: i.content
+    }))
+    if (kownladge.length > 0) {
+        let prompt = app.zsk_answer_prompt + '\n' +
+            kownladge.map((e, i) => i + 1 + "." + e.content).join('\n') + "\n问题：" + Q
+        await send(prompt, keyword = Q, show = true, sources = kownladge)
+    } else {
+        app.chat.pop()
+        sources = [{
+            title: '未匹配到知识库',
+            content: '本次对话内容完全由模型提供'
+        }]
+        return await send(Q, keyword = Q, show = true, sources = sources)
+    }
+}
 func.push({
     name: "快速知识库",
-    question: async () => {
-        let Q = app.question
-
-        // lsdh(false)
-        kownladge = (await find(Q, app.zsk_step)).map(i => ({
-            title: get_title_form_md(i.title),
-            url: get_url_form_md(i.title),
-            content: i.content
-        }))
-        if (kownladge.length > 0) {
-            let prompt = app.zsk_answer_prompt + '\n' +
-                kownladge.map((e, i) => i + 1 + "." + e.content).join('\n') + "\n问题：" + Q
-            await send(prompt, keyword = Q, show = true, sources = kownladge)
-        } else {
-            app.chat.pop()
-            sources = [{
-                title: '未匹配到知识库',
-                content: '本次对话内容完全由模型提供'
-            }]
-            return await send(Q, keyword = Q, show = true, sources = sources)
-        }
-    }
+    question: window.answer_with_fast_zsk
 }
 )
 if (app.llm_type == "rwkv") {
