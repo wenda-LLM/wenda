b809acfc49260ba4e3f80918eea0ec4990c35604
l15y <ly444983@live.com>
2023-06-19 21:55:47 +0800

    Update zhishiku_serper.py

--- plugins/zhishiku_serper.py
+++ plugins/zhishiku_serper.py
@@ -6,8 +6,6 @@ def find(search_query,step = 0):
     conn = http.client.HTTPSConnection("google.serper.dev")
     payload = json.dumps({
     "q": search_query,
-    "gl": "cn",
-    "hl": "zh-cn"
     })
     headers = {
     'X-API-KEY': SERPER_API_KEY,
