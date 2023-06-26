cb2674dd7ae5f41f5fb2a8a80bcb0a2fc9aeeafb
l15y <ly444983@live.com>
2023-06-26 17:18:54 +0800

    Update wdss.js

--- autos/wdss.js
+++ autos/wdss.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         猫猫也会的图块化编程
+// @name         闻达搜索
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  使用图块化编程的方式进行简单的auto开发
-// @author       You
+// @description  闻达搜索入口
+// @author       lyyyy
 // @match        http://127.0.0.1:17860/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=0.1
 // @grant        none
 // ==/UserScript==
 
-app.plugins.push({ icon: "file-search-outline", url: "wdss.html", hide_title: true })
+app.plugins.push({ icon: "file-search", url: "wdss.html", hide_title: true })
