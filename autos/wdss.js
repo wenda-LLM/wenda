23797b5ef6113b662bec7ec3860e9a490bc66bc2
l15y <ly444983@live.com>
2023-06-26 17:16:53 +0800

    优化闻达搜索

---
+++ autos/wdss.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         猫猫也会的图块化编程
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  使用图块化编程的方式进行简单的auto开发
+// @author       You
+// @match        http://127.0.0.1:17860/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=0.1
+// @grant        none
+// ==/UserScript==
+
+app.plugins.push({ icon: "file-search-outline", url: "wdss.html", hide_title: true })
