ff6665121289ff23c91cc62d52683a2ef115aa02
l15y <ly444983@live.com>
2023-07-14 21:30:39 +0800

    闻达论文

---
+++ autos/wdlw.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         闻达论文
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  闻达论文入口
+// @author       lyyyy
+// @match        http://127.0.0.1:17860/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=0.1
+// @grant        none
+// ==/UserScript==
+
+app.plugins.push({ icon: "file-document-edit", url: "wdlw.html", hide_title: true })
