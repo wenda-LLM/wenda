b921bafe3c5857afc0b0a4a44fa7566de37a72ae
l15y <ly444983@live.com>
2024-05-27 15:17:01 +0800

    修复chatglm1

--- llms/llm_glm6b.py
+++ llms/llm_glm6b.py
@@ -30,7 +30,7 @@ def chat_init(history):
 def chat_one(prompt, history_formatted, max_length, top_p, temperature, data):
     yield str(len(prompt))+'字正在计算'
     
-    if len(history_formatted)>0 and history_formatted[0]['role']=="system":
+    if len(history_formatted)>0 and chatglm3_mode and history_formatted[0]['role']=="system":
         if prompt.startswith("observation!"):
             prompt = prompt.replace("observation!", "")
             response, history = model.chat(tokenizer, prompt, history_formatted, role="observation",
