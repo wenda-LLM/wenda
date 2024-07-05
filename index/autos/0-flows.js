e36aeeecc3806fc346d7ae171746ba59e8eca068
l15y <ly444983@live.com>
2024-07-05 11:53:39 +0800

    -

--- index/autos/0-flows.js
+++ index/autos/0-flows.js
@@ -12,7 +12,7 @@
 if (typeof app.nodes == 'object') {
     app.nodes = app.nodes.concat([{
         name: '初始 prompt',
-        function: '`{{template}}`',
+        function: '{return [`{{template}}`]}',
         node: "prompt",
         icon: "ray-start",
         in: 0,
@@ -23,12 +23,12 @@ if (typeof app.nodes == 'object') {
     },
     {
         name: '单轮对话',
-        function: 'send(a)',
+        function: '{console.log(a);let rtn=send(a);console.dir([rtn]);return [rtn]}',
         node: "send",
         icon: "chat",
         in: 1,
         out: 1,
-        data: {  },
+        data: {},
         template: ``
     },
     {
@@ -54,6 +54,17 @@ if (typeof app.nodes == 'object') {
 <textarea df-template placeholder='参数：a,b,c'></textarea></div>`
     },
     {
+        name: '程序2_多输出',
+        function: '{{template}}',
+        node: "function2",
+        icon: "function-variant",
+        in: 2,
+        out:2,
+        data: { "template": "{return [a+'wenda',b+'我喜欢']}" },
+        template: `<div class="box">
+<textarea df-template placeholder='参数：a,b'></textarea></div>`
+    },
+    {
         name: '报警',
         function: 'alert(a)',
         node: "alert",
@@ -73,6 +84,7 @@ if (typeof app.nodes == 'object') {
         data: {},
         template: ``
     },
+    // {{template}}是上一个节点输入的数据？a b c 是用户输入到这个节点的三个数据，{1}、{2}、{3} 是用户输入到节点输入框中的数据 
     {
         name: '模板',
         function: '`{{template}}`.replace("{1}",a).replace("{2}",b).replace("{3}",c)',
@@ -81,9 +93,47 @@ if (typeof app.nodes == 'object') {
         in: 3,
         out: 1,
         data: { "template": '' },
-        template: `<div class="box">输入参数：{1}{2}{3}
-<textarea df-template placeholder='输入内容{1}{2}{3}'></textarea></div>`
+        template: `<div class="box">Ger Vars
+<textarea df-template placeholder='输入内容{1}{2}{3}'></textarea>
+Output template with vars</div>`
     },
-])
+    {
+        name: '对话(LLMs可选)',
+        function: 'send(a,b)',
+        node: "chatbyllm",
+        icon: "chat",
+        in: 2,
+        out: 1,
+        data: { "channel": 'channel_3'},
+        template:  `
+          <div>
+            <div class="title-box"><i class="fab fa-telegram-plane"></i> Telegram bot</div>
+            <div class="box">
+              <p>调用大模型生成文本</p>
+              <p>请在两个输入端分别输入系统提示词和用户提示词</p>
+              <p>请选择大语言模型</p>
+              <select df-channel>
+                <option value="channel_1">chatGLM2</option>
+                <option value="channel_2">chatGLM3</option>
+                <option value="channel_3">Qwen2-0.5B</option>
+                <option value="channel_4">Qwen2-7B</option>
+              </select>
+            </div>
+          </div>
+          `
+    },
+    {
+        name: '代码块',
+        function: '{{template}}',
+        node: "code_block",
+        icon: "text-box-plus",
+        in: 1,
+        out: 1,
+        data: { "template": 'alert(`这是一段警告框代码！`);send(a)' },
+        template: `<div class="box">请输入源码
+<textarea df-template placeholder='alert("这是一段警告框代码！");send(a)'></textarea>
+</div>`
+    },
+    ])
 
 }
